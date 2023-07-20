b8d10f4a585c39d99d7b187a165c181b05d370ff
Parag Vora <59291253+pcvora23@users.noreply.github.com>
2023-07-20 13:50:39 +0000

    Http basics and started express

--- 2-Express/app.js
+++ 2-Express/app.js
@@ -1,1 +1,18 @@
-console.log('express js')
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Home Page')
+})
+app.get('/about', (req, res) => {
+  res.status(200).send('About page')
+})
+
+app.all('*',(req,res)=>{
+  res.status(404).send('<h2>Resource not found</h2>')
+})
+
+app.listen(5000, () => [
+  console.log(`server is listening on port 5000...`)
+])
