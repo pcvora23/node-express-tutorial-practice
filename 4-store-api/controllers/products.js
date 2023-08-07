359209f66b204cd0ee4cc3f21d05c15f5241f867
Parag Vora <parag.intelivita@gmail.com>
2023-08-08 00:01:31 +0530

    sort,limit,skip,select,etc..

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -1,10 +1,10 @@
 const Product = require('../models/product')
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ featured: true })
+    const products = await Product.find({}).select('name price').limit(10).skip(1)
     res.status(200).json({ nbHits: products.length, products })
 }
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort,fields } = req.query
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -15,7 +15,24 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject);
+    // sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result =  result.sort(sortList);
+    } else {
+        result =  result.sort('createdAt');
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result =  result.select(fieldsList);
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+    const products = await result;
     res.status(200).json({ nbHits: products.length, products })
 }
 
